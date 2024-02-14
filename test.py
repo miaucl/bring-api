0fb7908de6a0156e1e1ea3e0c974bbfcba7b1fb6
tr4nt0r <manni@zapto.de>
2024-02-14 19:49:40 +0100

    some small fixes
    * renamed reserved variable list
    * replaced fix interpreter path with env variable
    * fixed type

--- test.py
+++ test.py
@@ -15,28 +15,28 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 load_dotenv()
 
 
-async def test_add_complete_remove(bring: Bring, list: BringList):
+async def test_add_complete_remove(bring: Bring, lst: BringList):
     """Test add-complete-remove for an item."""
 
     # Save an item with specifications to a certain shopping list
-    await bring.saveItem(list["listUuid"], "Äpfel", "low fat")
+    await bring.saveItem(lst["listUuid"], "Äpfel", "low fat")
 
     # Get all the pending items of a list
-    items = await bring.getItems(list["listUuid"])
+    items = await bring.getItems(lst["listUuid"])
     logging.info("List purchase items: %s", items["purchase"])
 
     # Check of an item
-    await bring.completeItem(list["listUuid"], items["purchase"][0]["itemId"])
+    await bring.completeItem(lst["listUuid"], items["purchase"][0]["itemId"])
 
     # Get all the recent items of a list
-    items = await bring.getItems(list["listUuid"])
+    items = await bring.getItems(lst["listUuid"])
     logging.info("List recently items: %s", items["recently"])
 
     # Remove an item from a list
-    await bring.removeItem(list["listUuid"], "Äpfel")
+    await bring.removeItem(lst["listUuid"], "Äpfel")
 
     # Get all the items of a list
-    items = await bring.getItems(list["listUuid"])
+    items = await bring.getItems(lst["listUuid"])
     logging.info("List all items: %s / %s", items["purchase"], items["recently"])
 
 
@@ -50,10 +50,10 @@ async def main():
 
         # Get information about all available shopping list and select one to test with
         lists = (await bring.loadLists())["lists"]
-        list = next(list for list in lists if list["name"] == os.environ["LIST"])
-        logging.info("Selected list: %s (%s)", list["name"], list["listUuid"])
+        lst = next(lst for lst in lists if lst["name"] == os.environ["LIST"])
+        logging.info("Selected list: %s (%s)", lst["name"], lst["listUuid"])
 
-        await test_add_complete_remove(bring, list)
+        await test_add_complete_remove(bring, lst)
 
 
 asyncio.run(main())
