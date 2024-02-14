c4ce74dd5cef73f98a3e835fff27e920c0f851b1
tr4nt0r <manni@zapto.de>
2024-02-14 23:40:35 +0100

    add test for notify

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import aiohttp
 from dotenv import load_dotenv
 
 from bring_api.bring import Bring
-from bring_api.types import BringList
+from bring_api.types import BringList, BringNotificationType
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -40,6 +40,18 @@ async def test_add_complete_remove(bring: Bring, lst: BringList):
     logging.info("List all items: %s / %s", items["purchase"], items["recently"])
 
 
+async def test_push_notifications(bring: Bring, lst: BringList):
+    """Test sending push notifications."""
+
+    # Send a going shopping notification
+    await bring.notify(lst["listUuid"], BringNotificationType.GOING_SHOPPING, "")
+
+    # Send a urgent message with argument item name
+    await bring.notify(
+        lst["listUuid"], BringNotificationType.URGENT_MESSAGE, "Pouletbrüstli"
+    )
+
+
 async def main():
     """Test Bring API."""
     async with aiohttp.ClientSession() as session:
@@ -54,6 +66,8 @@ async def main():
         logging.info("Selected list: %s (%s)", lst["name"], lst["listUuid"])
 
         await test_add_complete_remove(bring, lst)
+
+        await test_push_notifications(bring, lst)
 
 
 asyncio.run(main())
