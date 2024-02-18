33785c0c318dfb46ad842378b68ee1848f768e14
tr4nt0r <manni@zapto.de>
2024-02-18 05:13:02 +0100

    add more tests

--- tests/test_bring.py
+++ tests/test_bring.py
@@ -1,4 +1,5 @@
 """Unit tests for bring-api."""
+import enum
 import os
 import secrets
 import uuid
@@ -12,6 +13,7 @@ from bring_api.exceptions import (
     BringEMailInvalidException,
     BringUserUnknownException,
 )
+from bring_api.types import BringNotificationType
 
 load_dotenv()
 
@@ -74,8 +76,87 @@ class TestLogin:
         assert str(uuid.UUID(data["publicUuid"])) == data["publicUuid"], "uuid invalid"
 
 
+async def test_load_lists(bring):
+    """Test load_lists."""
+    await bring.login()
+    lists = await bring.load_lists()
+    assert isinstance(lists, dict) is True, "load_lists must return a TypedDict"
+    assert len(lists["lists"]) > 0, "lists count is 0"
+    assert next(
+        (lst for lst in lists["lists"] if lst["name"] == os.environ["LIST"]), False
+    ), f"Test List '{os.environ["LIST"]}' not found"
+    for lst in lists["lists"]:
+        assert (
+            str(uuid.UUID(lst["listUuid"])) == lst["listUuid"]
+        ), f"not a valid uuid {lst["listUuid"]}"
+        assert len(lst["name"]) > 0, "name cannot be empty"
+
+
 class TestNotifications:
     """Tests for notification method."""
 
-    def test_notify_going_shopping(self, bring):
+    @pytest.mark.parametrize(
+        ("notification_type", "item_name"),
+        [
+            (BringNotificationType.GOING_SHOPPING, ""),
+            (BringNotificationType.CHANGED_LIST, ""),
+            (BringNotificationType.SHOPPING_DONE, ""),
+            (BringNotificationType.URGENT_MESSAGE, "WITH_ITEM_NAME"),
+        ],
+    )
+    async def test_notify(
+        self,
+        bring,
+        notification_type: BringNotificationType,
+        item_name: str,
+    ):
         """Test GOING_SHOPPING notification."""
+
+        await bring.login()
+        lists = (await bring.load_lists())["lists"]
+        lst = next(lst for lst in lists if lst["name"] == os.environ["LIST"])
+        resp = await bring.notify(lst["listUuid"], notification_type, item_name)
+        assert resp.status == 200
+
+    async def test_notify_urgent_message_item_name_missing(self, bring):
+        """Test URGENT_MESSAGE notification."""
+
+        await bring.login()
+        lists = (await bring.load_lists())["lists"]
+        lst = next(lst for lst in lists if lst["name"] == os.environ["LIST"])
+        with pytest.raises(
+            ValueError,
+            match="notificationType is URGENT_MESSAGE but argument itemName missing.",
+        ):
+            await bring.notify(
+                lst["listUuid"], BringNotificationType.URGENT_MESSAGE, ""
+            )
+
+    async def test_notify_notification_type_raise_attribute_error(self, bring):
+        """Test URGENT_MESSAGE notification."""
+
+        await bring.login()
+        lists = (await bring.load_lists())["lists"]
+        lst = next(lst for lst in lists if lst["name"] == os.environ["LIST"])
+        with pytest.raises(
+            AttributeError,
+        ):
+            await bring.notify(lst["listUuid"], "STRING", "")
+
+    async def test_notify_notification_type_raise_type_error(self, bring):
+        """Test URGENT_MESSAGE notification."""
+
+        class WrongEnum(enum.Enum):
+            """Test Enum."""
+
+            UNKNOWN = "UNKNOWN"
+
+        await bring.login()
+        lists = (await bring.load_lists())["lists"]
+        lst = next(lst for lst in lists if lst["name"] == os.environ["LIST"])
+        with pytest.raises(
+            TypeError,
+            match="notificationType WrongEnum.UNKNOWN not supported,"
+            "must be of type BringNotificationType.",
+        ):
+            await bring.notify(lst["listUuid"], WrongEnum.UNKNOWN, "")
