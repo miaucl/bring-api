586d4118ad8500af85699c9cfc3addb653c5dfbf
Mr. Bubbles <manni@zapto.de>
2024-02-20 06:37:29 +0100

    define url and header as constants (#16)

---
+++ bring_api/const.py
@@ -0,0 +1,12 @@
+"""Constants for bring-api."""
+from typing import Final
+
+API_BASE_URL: Final = "https://api.getbring.com/rest/"
+DEFAULT_HEADERS: Final = {
+    "Authorization": "Bearer",
+    "X-BRING-API-KEY": "cof4Nc6D8saplXjE3h3HXqHH8m7VU2i1Gs0g85Sp",
+    "X-BRING-CLIENT": "android",
+    "X-BRING-APPLICATION": "bring",
+    "X-BRING-COUNTRY": "DE",
+    "X-BRING-USER-UUID": "",
+}
