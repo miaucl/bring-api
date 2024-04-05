55a093707ba7f7f83e0b73383fe66f7cf906f164
tr4nt0r <manni@zapto.de>
2024-04-05 04:28:33 +0200

    Add tests for loading translations

--- tests/test_bring.py
+++ tests/test_bring.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import pytest
 
 from bring_api.bring import Bring
+from bring_api.const import BRING_SUPPORTED_LOCALES
 from bring_api.exceptions import (
     BringAuthException,
     BringEMailInvalidException,
@@ -612,3 +613,138 @@ class TestCompleteItem:
             exc.value.args[0] == f"Completing item item_name from list {UUID} "
             "failed due to request exception."
         )
+
+
+class TestLoadArticleTranslations:
+    """Test loading of article translation tables."""
+
+    def mocked__load_article_translations_from_file(self, locale):
+        """Mock and raise for fallback to ressource download."""
+        raise OSError()
+
+    def test_load_file(self, bring, mocked):
+        """Test loading json from file."""
+
+        dictionary = bring._Bring__load_article_translations_from_file("de-CH")
+
+        assert "Pouletbrüstli" in dictionary
+        assert dictionary["Pouletbrüstli"] == "Pouletbrüstli"
+        assert len(dictionary) == 444
+
+    async def test_load_from_list_article_language(self, bring, monkeypatch):
+        """Test loading json from listArticleLanguage."""
+
+        monkeypatch.setattr(
+            bring, "user_list_settings", {UUID: {"listArticleLanguage": "de-DE"}}
+        )
+
+        dictionaries = await bring._Bring__load_article_translations()
+
+        assert "de-DE" in dictionaries
+        assert dictionaries["de-DE"]["Pouletbrüstli"] == "Hähnchenbrust"
+        assert len(dictionaries["de-DE"]) == 444
+
+    async def test_load_from_user_locale(self, bring, monkeypatch):
+        """Test loading json from user_locale."""
+
+        monkeypatch.setattr(bring, "user_locale", "de-DE")
+
+        dictionaries = await bring._Bring__load_article_translations()
+
+        assert "de-DE" in dictionaries
+        assert dictionaries["de-DE"]["Pouletbrüstli"] == "Hähnchenbrust"
+        assert len(dictionaries["de-DE"]) == 444
+
+    @pytest.mark.parametrize(
+        ("test_locale", "expected_locale"),
+        [
+            ("de-XX", "de-DE"),
+            ("en-XX", "en-US"),
+            ("es-XX", "es-ES"),
+            ("de-DE", "de-DE"),
+            ("en-GB", "en-GB"),
+        ],
+    )
+    async def test_map_user_language_to_locale(
+        self, bring, test_locale, expected_locale
+    ):
+        """Test mapping invalid user_locale to valid locale."""
+
+        user_locale = {"language": test_locale[0:2], "country": test_locale[3:5]}
+        locale = bring.map_user_language_to_locale(user_locale)
+
+        assert expected_locale == locale
+
+    async def test_load_all_locales(self, bring, monkeypatch):
+        """Test loading all locales."""
+
+        user_list_settings = {
+            k: {"listArticleLanguage": v} for k, v in enumerate(BRING_SUPPORTED_LOCALES)
+        }
+
+        monkeypatch.setattr(bring, "user_list_settings", user_list_settings)
+        dictionaries = await bring._Bring__load_article_translations()
+
+        assert len(dictionaries) == 19  # de-CH is skipped
+
+    async def test_load_fallback_to_download(self, bring, mocked, monkeypatch):
+        """Test loading json and fallback to download from web."""
+        mocked.get(
+            "https://web.getbring.com/locale/articles.de-DE.json",
+            payload={"test": "test"},
+            status=200,
+        )
+
+        monkeypatch.setattr(bring, "user_locale", "de-DE")
+
+        monkeypatch.setattr(
+            Bring,
+            "_Bring__load_article_translations_from_file",
+            self.mocked__load_article_translations_from_file,
+        )
+
+        dictionaries = await bring._Bring__load_article_translations()
+
+        assert dictionaries["de-DE"] == {"test": "test"}
+
+    @pytest.mark.parametrize(
+        "exception",
+        [
+            asyncio.TimeoutError,
+            aiohttp.ClientError,
+        ],
+    )
+    async def test_request_exceptions(self, bring, mocked, monkeypatch, exception):
+        """Test loading json and fallback to download from web."""
+        mocked.get(
+            "https://web.getbring.com/locale/articles.de-DE.json", exception=exception
+        )
+
+        monkeypatch.setattr(bring, "user_locale", "de-DE")
+
+        monkeypatch.setattr(
+            Bring,
+            "_Bring__load_article_translations_from_file",
+            self.mocked__load_article_translations_from_file,
+        )
+        with pytest.raises(BringRequestException):
+            await bring._Bring__load_article_translations()
+
+    async def test_parse_exception(self, bring, mocked, monkeypatch):
+        """Test loading json and fallback to download from web."""
+        mocked.get(
+            "https://web.getbring.com/locale/articles.de-DE.json",
+            status=200,
+            body="not json",
+            content_type="application/json",
+        )
+
+        monkeypatch.setattr(bring, "user_locale", "de-DE")
+
+        monkeypatch.setattr(
+            Bring,
+            "_Bring__load_article_translations_from_file",
+            self.mocked__load_article_translations_from_file,
+        )
+        with pytest.raises(BringParseException):
+            await bring._Bring__load_article_translations()
