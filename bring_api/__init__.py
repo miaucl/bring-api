9961c99a1a5f9075e97de3cb100b667b9b4fac5a
tr4nt0r <manni@zapto.de>
2024-06-16 01:37:03 +0200

    Declare public API in init module
    - Imported `Bring` class and various exceptions and types from their respective modules.
    - Defined `__all__` to explicitly declare the public API of the package.
    
    These changes ensure a cleaner and more structured initialization, making it easier to utilize the available components of the Bring API package.
    
    Also these changes will allow the scanning and creation of documentation for mkdocs

--- bring_api/__init__.py
+++ bring_api/__init__.py
@@ -1,1 +1,51 @@
 """Bring API package."""
+
+from .bring import Bring
+from .exceptions import (
+    BringAuthException,
+    BringEMailInvalidException,
+    BringParseException,
+    BringRequestException,
+    BringTranslationException,
+    BringUserUnknownException,
+)
+from .types import (
+    BringAuthResponse,
+    BringAuthTokenRespone,
+    BringItem,
+    BringItemOperation,
+    BringItemsResponse,
+    BringListItemDetails,
+    BringListItemsDetailsResponse,
+    BringListResponse,
+    BringNotificationsConfigType,
+    BringNotificationType,
+    BringSyncCurrentUserResponse,
+    BringUserListSettingEntry,
+    BringUserSettingsEntry,
+    BringUserSettingsResponse,
+)
+
+__all__ = [
+    "Bring",
+    "BringAuthException",
+    "BringAuthResponse",
+    "BringAuthTokenRespone",
+    "BringEMailInvalidException",
+    "BringItem",
+    "BringItemOperation",
+    "BringItemsResponse",
+    "BringListItemDetails",
+    "BringListItemsDetailsResponse",
+    "BringListResponse",
+    "BringNotificationsConfigType",
+    "BringNotificationType",
+    "BringParseException",
+    "BringRequestException",
+    "BringSyncCurrentUserResponse",
+    "BringTranslationException",
+    "BringUserListSettingEntry",
+    "BringUserSettingsEntry",
+    "BringUserSettingsResponse",
+    "BringUserUnknownException",
+]
